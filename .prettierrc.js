5734ce41284fc5aa57c074170b9f1870bf85b65f
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-07 19:28:31 +0545

    Configure Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'crlf',
+  singleQuote: true,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  jsxBracketSameLine: false
+};
