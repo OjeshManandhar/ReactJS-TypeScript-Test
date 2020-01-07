fa212e15c9a46b482fbb50027be75443821aed82
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-07 19:53:22 +0545

    Add common styles

---
+++ src/common/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  padding: 0;
+  margin: 0;
+`;
+
+export const LongDiv = styled.div`
+  height: 1000px;
+  width: 100%;
+`;
