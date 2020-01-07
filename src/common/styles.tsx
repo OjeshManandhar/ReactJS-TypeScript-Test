7d1596f389e24deefc64ff2dd50a8d9b28c01a99
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-07 21:47:08 +0545

    Remove unnecessary files and add stupid styles

--- src/common/styles.tsx
+++ src/common/styles.tsx
@@ -5,9 +5,17 @@ export const Container = styled.div`
   width: 100vw;
   padding: 0;
   margin: 0;
+
+  border: 5px solid red;
+
+  overflow: auto;
 `;
 
 export const LongDiv = styled.div`
   height: 1000px;
   width: 100%;
+
+  border: 5px solid green;
+
+  background: #eee;
 `;
