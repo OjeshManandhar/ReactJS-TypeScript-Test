e967e4e8448bc9731b27284297d43eb090b876d8
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-07 20:00:13 +0545

    Add dummy content

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// styles
+import * as S from 'common/styles';
+
+function Home() {
+  return (
+    <S.Container>
+      <S.LongDiv>
+        Home page <br /> Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Amet magni cumque aspernatur animi reiciendis reprehenderit.
+      </S.LongDiv>
+    </S.Container>
+  );
+}
+
+export default Home;
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import * as S from 'common/styles';
+
+function Register() {
+  return (
+    <S.Container>
+      <S.LongDiv>
+        Regiset page <br /> Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Laboriosam, sit. Deserunt voluptatibus accusantium magni soluta
+        adipisci consectetur ullam eligendi reiciendis.
+      </S.LongDiv>
+    </S.Container>
+  );
+}
+
+export default Register;
