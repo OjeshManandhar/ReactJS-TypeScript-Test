25edeae34deed08327133cc24dc87eda2939a3dc
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-12 17:20:00 +0545

    temp commit

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,20 +1,61 @@
 import React from 'react';
 
+// packages
+import scrollIntoView from 'scroll-into-view-if-needed';
+
+// context
+import NavBarContext from 'context/navBarContext/context';
+
+// assets
+import logo from 'assets/logo.png';
+
+// styles
 import * as Nav from './styles';
 
-import logo from 'assets/logo.png';
+function scrollToDiv(ref: React.RefObject<HTMLDivElement>) {
+  console.log('ref.current:', ref.current);
+  console.log('ref:', ref);
+
+  // scrollIntoView(ref.current, {
+  //   behavior: 'smooth',
+  //   block: 'end',
+  //   inline: 'nearest'
+  // });
+
+  // ref.current.scrollIntoView({ block: 'end', behavior: 'smooth' });
+
+  // ref.current.scrollIntoView({
+  //   behavior: 'smooth',
+  //   block: 'end',
+  //   inline: 'nearest'
+  // });
+}
 
 function NavBar() {
   return (
-    <Nav.Container>
-      <Nav.Logo src={logo} />
-      <Nav.Navs>
-        <Nav.NavLinks>Home</Nav.NavLinks>
-        <Nav.NavLinks>About</Nav.NavLinks>
-        <Nav.NavLinks>Product</Nav.NavLinks>
-        <Nav.NavLinks>Contact</Nav.NavLinks>
-      </Nav.Navs>
-    </Nav.Container>
+    <NavBarContext.Consumer>
+      {context => {
+        return (
+          <Nav.Container>
+            <Nav.Logo src={logo} />
+            <Nav.Navs>
+              <Nav.NavLinks onClick={() => scrollToDiv(context.home)}>
+                Home
+              </Nav.NavLinks>
+              <Nav.NavLinks onClick={() => scrollToDiv(context.about)}>
+                About
+              </Nav.NavLinks>
+              <Nav.NavLinks onClick={() => scrollToDiv(context.product)}>
+                Product
+              </Nav.NavLinks>
+              <Nav.NavLinks onClick={() => scrollToDiv(context.contact)}>
+                Contact
+              </Nav.NavLinks>
+            </Nav.Navs>
+          </Nav.Container>
+        );
+      }}
+    </NavBarContext.Consumer>
   );
 }
 
--- src/components/NavBar/styles.tsx
+++ src/components/NavBar/styles.tsx
@@ -35,6 +35,7 @@ export const NavLinks = styled.span`
 
   :hover {
     color: blueviolet;
+    cursor: pointer;
 
     ::before {
       content: '[';
